4652c89dae6c38d79d74129569761ef7c859dbc4
Mr. Dung <dung13890@gmail.com>
2017-08-19 09:41:05 +0700

    Fix serve.sh

--- serve.sh
+++ serve.sh
@@ -11,29 +11,39 @@ block="server {
     listen 80;
     server_name $2;
     root $3;
+
     index index.html index.htm index.php;
     charset utf-8;
+
     location / {
         try_files \$uri \$uri/ /index.php?\$query_string;
     }
+
     location = /favicon.ico { access_log off; log_not_found off; }
     location = /robots.txt  { access_log off; log_not_found off; }
+
     # set client body size to 500M #
     client_max_body_size 500M;
+
+    # Increase buffer size to deal with too long URL (especially on redirect)
     fastcgi_buffers 8 16k;
     fastcgi_buffer_size 32k;
-    sendfile off;
-    location ~ \.php$ {
-        fastcgi_split_path_info ^(.+\.php)(/.+)$;
 
+    sendfile off;
+
+    # pass the PHP scripts to FastCGI server listening on the php-fpm socket
+    location ~ \.php$ {
+        fastcgi_split_path_info ^(.+?\.php)(/.*)$;
+        if (!-f \$document_root\$fastcgi_script_name) {
+            return 404;
+        }
         include fastcgi_params;
         fastcgi_pass php-upstream;
         fastcgi_index index.php;
-        fastcgi_param  SCRIPT_FILENAME  /scripts$fastcgi_script_name;
+        fastcgi_param SCRIPT_FILENAME \$realpath_root\$fastcgi_script_name;
+        fastcgi_param DOCUMENT_ROOT \$realpath_root;
+        fastcgi_param REALPATHTEST \$realpath_root;
         internal;
-    }
-    location ~ /\.ht {
-        deny all;
     }
 }
 "
